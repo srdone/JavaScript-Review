68c2a0f5504f3bda3d1b3a4a5e7a1bb0f2059e4b
srdone <aDT-ph7-5Ew-qQF>
2014-12-23 20:23:37 -0700

    First two problems completed

--- arrayReview.js
+++ arrayReview.js
@@ -3,7 +3,9 @@ var threeItems = [1,2,3];
 //alert the result of your function
 
   //code here
-
+function last() {
+  console.log(threeItems[threeItems.length - 1]);
+}
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -14,7 +16,12 @@ var threeItems = [1,2,3];
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
   //code here
-
+for (var i = 0; i < evenArray.length; i++) {
+  if (evenArray[i] % 2 !== 0) {
+    evenArray.splice(i,i);
+  }
+}
+console.log(evenArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -23,11 +30,22 @@ var evenArray = [1,2,3,6,22,98,45,23,22,12];
 //below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
-}
+};
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
+function alertInRandomArray(arr) {
+  var rnum = getRandomArbitrary();
+  var result = false;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === rnum) {
+      result = true;
+    }
+  }
+  console.log('The random number' + ' ' + rnum + ' ' + (result ? 'is in' : 'is not in') + ' ' + 'the array.');
+}
 
+alertInRandomArray(randomArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
