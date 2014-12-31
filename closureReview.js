7efa7eb6df95f83d8fb8f29c670c21ba869043cc
srdone <aDT-ph7-5Ew-qQF>
2014-12-31 06:55:36 -0700

    Completed closureReview.js

--- closureReview.js
+++ closureReview.js
@@ -10,6 +10,8 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //code here
+  // The callFriend function returns a function, which must then be called with the phone number as the parameter
+  console.log(callFriend()('435-215-9248'));
 
 
 
@@ -20,3 +22,28 @@ Write a function that accepts a function as it's only argument and returns a new
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 
 */
+
+function limit(func, lim) {
+  var timesCalled = 0;
+  function limited() {
+    if (timesCalled < lim) {
+      timesCalled++;
+      func();
+    }
+    else {
+      console.log('STAHHP');
+    }
+  }
+  return limited;
+}
+
+function sayHa() {
+  console.log('HA!');
+}
+
+var limitedHa = limit(sayHa, 2);
+
+limitedHa();
+limitedHa();
+limitedHa();
+limitedHa();
